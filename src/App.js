1d1f1c6a1816b148d072f606ed44bf5925de8e8f
ClarkeTechnology <clarketechnologyltd@gmail.com>
2022-07-21 11:27:57 +0100

    Added cart modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
-  return (
-    <CartProvider>
-      <Header />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
-  );
+    return (
+        <CartProvider>
+            <Cart/>
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
+        </CartProvider>
+    );
 }
 
 export default App;
